266e8d925e8f90488ebc2c4b9abb4b639d0c7636
Quddus1916 <quddusjunior1916@gmail.com>
2022-08-19 00:06:57 -0500

    testing undefined type

--- main.go
+++ main.go
@@ -1,58 +1,48 @@
 package Config
 
 import (
-	//"encoding/json"
-	"fmt"
-	//"io/ioutil"
-	//"log"
+	//"fmt"
+	"github.com/spf13/viper"
 	"strings"
-	//"reflect"
-	"bufio"
-	"os"
-	//"github.com/spf13/viper"
 )
 
-type Config struct {
-	Key1 string
-	Key2 string
-	Key3 Key
-}
+// type Config struct {
+// 	Port         string `mapstructure:"PORT"`
+// 	SecretKey    string `mapstructure:"SECRETKEY"`
+// 	SqlUri       string `mapstructure:"SQL_URI"`
+// 	SqlDb        string `mapstructure:"SQL_DB_NAME"`
+// 	Username     string `mapstructure:"USER_NAME"`
+// 	Email        string `mapstructure:"EMAIL"`
+// 	SmtpHost     string `mapstructure:"SMTP_HOST"`
+// 	SmtpPort     string `mapstructure:"SMTP_PORT"`
+// 	SmtpPassword string `mapstructure:"SMTP_PASSWORD"`
+// 	Link         string `mapstructure:"LINK"`
+// }
 
-type Key struct {
-	key4 int
-	key5 string
-}
+func NewConfig(config_struct Config, filename string) (Config, error) {
+	viper.AddConfigPath(".")
+	file_info := strings.Split(filename, ".")
+	viper.SetConfigName(file_info[0])
+	viper.SetConfigType(file_info[1])
+	viper.AutomaticEnv()
 
-func SearchKey(filename string, key string) string {
-	readFile, err := os.Open(filename)
+	if err := viper.ReadInConfig(); err != nil {
+		return config_struct, err
+	}
+
+	err := viper.Unmarshal(&config_struct)
 	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	var fileLines []string
-
-	for fileScanner.Scan() {
-		fileLines = append(fileLines, fileScanner.Text())
+		return config_struct, err
 	}
 
-	readFile.Close()
+	return config_struct, err
 
-	for _, line := range fileLines {
-		res := strings.Split(line, ":")
-		value := res[0]
-		if len(value) > 1 {
-			storedkey := value[1 : len(value)-1]
-			if storedkey == key {
-				return line
-			}
-		}
-
-	}
-	return ""
 }
 
 // func main() {
-// 	line := SearchKey("app.env", "Key1")
-// 	fmt.Println(line)
+// 	config, err := NewConfig(Config{}, "app.env")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	fmt.Println(config.Port)
 // }
