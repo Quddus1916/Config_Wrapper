49ea7841b5a2ab86dd6517bbce481319e466730b
Quddus1916 <quddusjunior1916@gmail.com>
2022-08-31 00:21:03 -0500

    search in root if file not exist in path

--- config.go
+++ config.go
@@ -1,13 +1,23 @@
-package config
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
 	"reflect"
+	//"regexp"
 	"strconv"
+	"sync"
 )
+
+var m sync.Mutex
+var ConfigFile string
+var File_Extension string
+var Count_config_file []string
 
 type GetConfigParamAsString func(string, *string, string) string
 type GetConfigParamAsInt64 func(string, *string, string) int64
@@ -27,17 +37,9 @@ var Pair []KeyFunc
 const bitSize = 64 // Don't think about it to much. It's just 64 bits.
 var MapConfig map[string]interface{}
 var MapJson map[string]interface{}
+var OldConfig = make(map[string]interface{})
 
-func Common(key string, deep_key *string, default_val string) interface{} {
-
-	value, found := MapConfig[key]
-	if !found {
-		return default_val
-	}
-	if deep_key == nil {
-		return value
-	}
-
+func Decode(value interface{}) {
 	b, err := json.Marshal(value)
 	if err != nil {
 		panic(err)
@@ -46,43 +48,115 @@ func Common(key string, deep_key *string, default_val string) interface{} {
 	if err != nil {
 		fmt.Println("error:", err)
 	}
+}
+
+func Common(key string, deep_key *string, default_val string) interface{} {
+	var old_value interface{}
+	value, found := MapConfig[key]
+	if !found {
+		old_value, found = OldConfig[key]
+		if !found {
+			return default_val
+		}
+		return old_value
+	}
+	if deep_key == nil {
+		return value
+	}
+
+	Decode(value)
 
 	deep_value, found := MapJson[*deep_key]
 	if !found {
-		return default_val
+		Decode(old_value)
+		deep_value_old, found := MapJson[*deep_key]
+		if !found {
+
+			return default_val
+		}
+		return deep_value_old
 	}
 
 	return deep_value
 }
 
-func MisMatchedKey(old map[string]interface{}, updated map[string]interface{}) string {
+func MisMatchedKey(old map[string]interface{}, updated map[string]interface{}) []string {
+	var keys []string
 	for k, _ := range old {
 		eq := reflect.DeepEqual(old[k], updated[k])
 		if eq {
 
 		} else {
-			return k
+			keys = append(keys, k)
 		}
 	}
-	return ""
+	return keys
 }
 
-func CallFuncIfExists(key string) bool {
-	for _, v := range Pair {
-		if v.Key == key {
-			v.CallBackFunc()
-			return true
+// func VisitFile(path string, info os.FileInfo, err error) error {
+
+// 	if err != nil {
+
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+
+// 	if info.IsDir() || filepath.Ext(path) != File_Extension {
+
+// 		return nil
+// 	}
+
+// 	reg, err2 := regexp.Compile(ConfigFile)
+
+// 	if err2 != nil {
+
+// 		return err2
+// 	}
+
+// 	if reg.MatchString(info.Name()) {
+// 		Count_config_file = append(Count_config_file, path)
+// 		if len(Count_config_file) != 1 {
+// 			panic("to many config files with same name")
+// 		}
+// 	}
+//
+
+// 	return nil
+// }
+
+func CallFuncIfExists(key []string) bool {
+	fmt.Println(key)
+	for _, K := range key {
+		for _, v := range Pair {
+			if v.Key == K {
+				v.CallBackFunc()
+
+			}
 		}
 	}
-	return false
+	return true
 }
 
-func InitConfig(filepath string, pair []KeyFunc) (*Config, error) {
+func InitConfig(file_path string, pair []KeyFunc) (*Config, error) {
+
+	//ConfigFile = file_name_with_extension
+	// File_Extension = filepath.Ext(file_name_with_extension)
+	// err := filepath.Walk(".", VisitFile)
+
+	// if err != nil {
+
+	// }
+	_, err := os.Open(file_path)
+	if err == nil {
+		viper.SetConfigFile(file_path)
+	} else {
+		filename := filepath.Base(file_path)
+		file := fmt.Sprintf("%s%s", "./", filename)
+		viper.SetConfigFile(file)
+	}
 
 	Pair = pair
 	var config = new(Config)
-
-	viper.SetConfigFile(filepath)
 	viper.AutomaticEnv()
 
 	if MapConfig == nil {
@@ -97,7 +171,6 @@ func InitConfig(filepath string, pair []KeyFunc) (*Config, error) {
 
 	viper.OnConfigChange(func(e fsnotify.Event) {
 
-		var OldConfig = make(map[string]interface{})
 		fmt.Println("Config file changed:", e.Name)
 
 		b, err := json.Marshal(MapConfig)
@@ -123,7 +196,7 @@ func InitConfig(filepath string, pair []KeyFunc) (*Config, error) {
 		fmt.Println("config updated & Checking for any call_back func")
 
 		key := MisMatchedKey(OldConfig, MapConfig)
-		if key != "" {
+		if key != nil {
 			ok := CallFuncIfExists(key)
 			if ok {
 				fmt.Println("ok")
@@ -169,4 +242,34 @@ func InitConfig(filepath string, pair []KeyFunc) (*Config, error) {
 
 	return config, nil
 
+}
+
+func Prints1() {
+	fmt.Println("db updated")
+}
+func Prints2() {
+	fmt.Println("app updated")
+}
+func Prints3() {
+	fmt.Println("key updated")
+}
+func main() {
+	e := echo.New()
+	var pair []KeyFunc
+	pair = []KeyFunc{
+		{Key: "db", CallBackFunc: Prints1},
+		{Key: "app", CallBackFunc: Prints2},
+		{Key: "v", CallBackFunc: Prints3},
+	}
+	config, err := InitConfig("./g/e/config.dev.json", pair)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	p := "port"
+	// val := config.GetConfigParamAsString("app", nil, "1010")
+	// fmt.Println(val)
+	val2 := config.GetConfigParamAsInt64("app", &p, "1010")
+	fmt.Println(val2)
+	e.Start(":8080")
 }
